da27bd8e821fbf07a790ef827b33311f9fbc75a1
Akshat Upadhyay <akshatupadhyay2006@gmail.com>
2025-08-25 11:11:03 +0530

    level2

---
+++ Assignment/TriangleArea.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter base in cm: ");
+        double base = sc.nextDouble();
+        System.out.print("Enter height in cm: ");
+        double height = sc.nextDouble();
+
+        double areaCm2 = 0.5 * base * height;
+        double areaIn2 = areaCm2 / (2.54 * 2.54);
+
+        System.out.println("Area of Triangle is " + areaCm2 + " square cm and " + areaIn2 + " square inches");
+    }
+}
